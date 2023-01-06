77fcab6146bc618d971dd1d9c52c9858b2ac8c42
Anup481 <44887342+Anup481@users.noreply.github.com>
2023-01-06 12:44:41 +0530

    Add files via upload

---
+++ android/src/main/java/com/reactnative/ivpusic/imagepicker/PickerProvider.java
@@ -0,0 +1,14 @@
+package com.reactnative.ivpusic.imagepicker;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * @author chengang
+ * @date 2020/11/6
+ * @email chenganghonor@gmail.com
+ * @QQ 1410488687
+ * @package_name com.reactnative.ivpusic.imagepicker
+ * @describe
+ */
+public class PickerProvider extends FileProvider {
+}
